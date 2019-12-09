5cf37de464ff777b04c5054548672686cf5d3a01
Kevin Park <keunsuk413@gmail.com>
2019-12-09 13:05:05 -0600

    added outline of the app

---
+++ store.js
@@ -0,0 +1,17 @@
+const STORE = {
+  questions: [
+    {
+      question: "Hemorrhage",
+      options: [
+        "Excessive amount of blood", 
+        "Bleeding, Blood Loss", 
+        "Blood in the stool", 
+        "Blood in the urine"
+      ],
+      answer: "Bleeding, Blood Loss"
+    },
+  ],
+
+  currentQuestion: 0,
+  score: 0
+};
