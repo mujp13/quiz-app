bf9d041ff105839712e82f396a08e3b57cc162d4
Kevin Park <keunsuk413@gmail.com>
2019-12-12 16:44:56 -0600

    edit

--- index.js
+++ index.js
@@ -20,60 +20,57 @@ function renderQuestion() {
           <p><strong>Q${num}</strong>: ${STORE[currentQuestion].question}</p>
           <label>
               <input type="radio" name="choice" value="a" required>${STORE[currentQuestion].options[0]}</label>
+              <div class="hidden"></div>
           <label>
               <input type="radio" name="choice" value="b">${STORE[currentQuestion].options[1]}</label>
+              <div class="hidden"></div>
           <label>
               <input type="radio" name="choice" value="c">${STORE[currentQuestion].options[2]}</label>
+              <div class="hidden"></div>
           <label>
               <input type="radio" name="choice" value="d">${STORE[currentQuestion].options[3]}</label>
-          <input type="submit" value="Submit" class="submitButton button">
+              <div class="hidden"></div>
+          <input type="submit" id="answer" class="submitButton button">
+          <button type ="button" id="next-question" tabindex="6"> Next Q's</button>
       </div>
   `);
   
   $('.questionBox').html(questionHtml);
   updateQuestionNum();
-  currentQuestion += 1;
-  num += 1;
+  $("#next-question").hide();
+  currentQuestion++;
+  num++;
 }
 
 function updateQuestionNum() {
-  // update question number and score
-  let clickCount = 1;
-  $('.question-number').text(clickCount);
-  $('.submitButton').click(function(event) {
-    clickCount += 1;
-    $('.question-number').text(clickCount);
-  });
-}
-
-function generateQuestion() {
-  //create question
+  let count = 0;
+  count += 1;
+  $('.question-number').text(count);
 }
 
 
 function handleAnswer() {
   //user click the radio button to select answer.
   //store that in a variable
-  $('.question').on("submit",'.submitButton', function(event) {
+  $('.questionBox').on("submit", '.question', function(event) {
     event.preventDefault();
     
-    let currentIndex = 0;
-    let score = 0;
-    let currentQuestion = STORE[currentIndex];
-    let selectedAnswer = $("input[name=choice]:checked").val();
+    let selected = $('input:checked').val();
+    let correct = currentQuestion.answer;
     
     //User gets the correct answer
-    if(selectedAnswer === currentQuestion.answer) {
-      //update score
-      $("input[name=choice]:checked").append('<p class=correct-ans>You got it right!</p>');
+    if(selected === correct) {
+      $('.hidden').text('You got it right!');
     }
     else {
-      $("input[name=choice]:checked").append(`<p class=wrong-ans>You got it wrong! The answer is ${current.Question.answer}.</p>`);
+      $('.hidden').text('You got it right!');
     }
-
-
+    //$('#answer').hide();
+    //$("input[type=radio]").attr('disabled', true);
+    //$('#next-question').show();
   });
 }
+
 
 
 function nextQuestionDecision() {
