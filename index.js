e8efe75ccd3f70913c18b679271bbd784e9b50e3
Kevin Park <keunsuk413@gmail.com>
2019-12-14 16:54:09 -0600

    Formatting, color of question and score

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 let qNum = 1;
-let questNum =0;
+let questNum = 0;
 let index = 0;
 let result = 0;
 let score = 0;
 
-console.log(qNum);
-console.log(index);
-console.log(result);
+
 
 function handleStartQuiz() {
   //Create an event listener for clicking on "Start Quiz" button.
@@ -17,31 +15,33 @@ function handleStartQuiz() {
     $('.question-number').text(qNum);
     renderQuestion();
   });
-
 }
 
-function renderQuestion() {  
+function renderQuestion() {
   const questionHtml = $(`
-      <div class="question">
-          <p><strong>Q${qNum}</strong>: ${STORE[index].question}</p>
-          <label>
-              <input type="radio" name="choice" id="a" required value="${STORE[index].options[0]}"> ${STORE[index].options[0]} </label>
-          <label>
-              <input type="radio" name="choice" id="b" value="${STORE[index].options[1]}"> ${STORE[index].options[1]} </label>
-          <label>
-              <input type="radio" name="choice" id="c" value="${STORE[index].options[2]}"> ${STORE[index].options[2]} </label>
-          <label>
-              <input type="radio" name="choice" id="d" value="${STORE[index].options[3]}"> ${STORE[index].options[3]} </label>
-          <input type="button" value="submit" id="answer" class="submitButton button">
-          <button type ="button" id="next-question" tabindex="6"> Next </button>
-      </div>
+    <div class="question">
+      <p><strong>Q${qNum}</strong>: ${STORE[index].question}</p>
+      <label> 
+        <input type="radio" name="choice" id="a" required value="${STORE[index].options[0]}"> ${STORE[index].options[0]}
+      </label>
+      <label>
+        <input type="radio" name="choice" id="b" value="${STORE[index].options[1]}"> ${STORE[index].options[1]} 
+      </label>
+      <label>
+        <input type="radio" name="choice" id="c" value="${STORE[index].options[2]}"> ${STORE[index].options[2]} 
+      </label>
+      <label>
+        <input type="radio" name="choice" id="d" value="${STORE[index].options[3]}"> ${STORE[index].options[3]} 
+      </label>
+      <input type="button" value="submit" id="answer" class="submitButton button">
+      <button type ="button" id="next-question" tabindex="6"> Next </button>
+    </div>
   `);
 
-   $('.questionBox').html(questionHtml);
-   $('#next-question').hide();
-   questNum++;
-   $('.question-number').text(questNum);
-
+  $('.questionBox').html(questionHtml);
+  $('#next-question').hide();
+  questNum++;
+  $('.question-number').text(questNum);
 }
 
 function handleAnswer() {
@@ -49,40 +49,40 @@ function handleAnswer() {
     event.preventDefault();
 
     let selected = $('input[name=choice]:checked').val();
-    console.log(selected);
-    let correct = STORE[index].answer;
     
+    let correct = STORE[index].answer;
+
     if (!selected) {
-      alert("Choose an option");
+      alert('Choose an option');
       return;
     }
-    if(selected === correct) {
-      $('input[name=choice]:checked').closest('label').append('<div class="right-answer">You got it right!</div>');
+    if (selected === correct) {
+      $('input[name=choice]:checked')
+        .closest('label')
+        .append('<div class="right-answer">You got it right!</div>');
       score++;
       $('.score').text(`${score}/${qNum}`);
-    }
-    else {
-      $('input[name=choice]:checked').closest('label').append(`<div class="wrong-answer">You got it wrong! <br>The answer is "${STORE[index].answer}"</br></div>`);
+    } else {
+      $('input[name=choice]:checked')
+        .closest('label')
+        .append(`<div class="wrong-answer">You got it wrong! <br>The answer is "${STORE[index].answer}"</br></div>`);
       $('.score').text(`${score}/${qNum}`);
     }
     $('#answer').hide();
-    $("input[type=radio]").attr('disabled', true);
+    $('input[type=radio]').attr('disabled', true);
     $('#next-question').show();
- 
   });
 }
 
-
 function handleNextQuestion() {
   $('.questionBox').on('click', '#next-question', function(event) {
-    if((index === STORE.length - 1)) {
+    if (index === STORE.length - 1) {
       //This means that the user tried all questions
       //Get the result page
       displayResults();
-    }
-    else {
+    } else {
       qNum++;
-      index++;  
+      index++;
       renderQuestion();
     }
   });
@@ -105,20 +105,17 @@ function restartQuiz() {
   });
 }
 
-
-
 function displayResults() {
-  let grade = score/qNum * 100;
+  let grade = (score / qNum) * 100;
   let resultText = 0;
-  if(grade >= 70) {
+  if (grade >= 70) {
     resultText = '<p style="color:green;"><b>You passed the quiz!</b></p>';
+  } else {
+    resultText = '<p style="color:red;">You failed. Please retake the quiz!</p>';
   }
-  else {
-    resultText = '<p style="color:red;">You failed. Please retake the quiz!</p>'
-  }
-  
+
   let resultHtml = $(`<section class="result-page">
-    <p>Your score is: ${score}/${qNum} <b>${score/qNum*100}%</b></p>
+    <p>Your score is: ${score}/${qNum} <b>${(score / qNum) * 100}%</b></p>
     ${resultText}
     <div class="result">
       <button type="button" id="restart" class="restartButton button">Restart Quiz</button>
@@ -128,13 +125,10 @@ function displayResults() {
   restartQuiz();
 }
 
-
-
 function makeQuiz() {
   handleStartQuiz();
   handleAnswer();
   handleNextQuestion();
-  
 }
 
 $(makeQuiz);
--- store.js
+++ store.js
@@ -1,166 +1,110 @@
 const STORE = [
-    {
-      question: "Which of the following is the best aerosolized bronchodilator for a patient with an acute asthma attack?",
-      options: [
-        "albuterol (proventil)", 
-        "ipratropium bromide (Atrovent)", 
-        "cromolyn sodium (Intal)", 
-        "budesonide/formoterol (Symbicort)"
-      ],
-      answer: "albuterol (proventil)"
-    },
+  {
+    question:
+      'Which of the following is the best aerosolized bronchodilator for a patient with an acute asthma attack?',
+    options: [
+      'albuterol (proventil)',
+      'ipratropium bromide (Atrovent)',
+      'cromolyn sodium (Intal)',
+      'budesonide/formoterol (Symbicort)'
+    ],
+    answer: 'albuterol (proventil)'
+  },
 
-    {
-      question: "Which of the following drugs is the most appropriate to sedate a 30-year-old patient with status asthmaticus and hypotension who is receiving mechanical ventilation?",
-      options: [
-        "vecuronium bromide (Norcuron)", 
-        "lorazepam (Ativan)", 
-        "morphine sulfate", 
-        "oxycodone HCL (oxycotin)"
-      ],
-      answer: "lorazepam (Ativan)"
-    },
+  {
+    question:
+      'Which of the following drugs is the most appropriate to sedate a 30-year-old patient with status asthmaticus and hypotension who is receiving mechanical ventilation?',
+    options: ['vecuronium bromide (Norcuron)', 'lorazepam (Ativan)', 'morphine sulfate', 'oxycodone HCL (oxycotin)'],
+    answer: 'lorazepam (Ativan)'
+  },
 
-    {
-      question: "A respiratory therapist is performing spirometry on a patient with pulmonary fibrosis. Which of the following devices would possibly be damaged by this procedure?",
-      options: [
-        "wrights respirometer", 
-        "peak flowmeter", 
-        "vortex shedding device", 
-        "water seal spirometer"
-      ],
-      answer: "wrights respirometer"
-    },
+  {
+    question:
+      'A respiratory therapist is performing spirometry on a patient with pulmonary fibrosis. Which of the following devices would possibly be damaged by this procedure?',
+    options: ['wrights respirometer', 'peak flowmeter', 'vortex shedding device', 'water seal spirometer'],
+    answer: 'wrights respirometer'
+  },
 
-    {
-      question: "A physiologic goal of CPAP in atelectasis is to increase:",
-      options: [
-        "tidal volume", 
-        "functional residual capacity", 
-        "inspiratory capacity", 
-        "minute alveolar ventilation"
-      ],
-      answer: "functional residual capacity"
-    },
+  {
+    question: 'A physiologic goal of CPAP in atelectasis is to increase:',
+    options: ['tidal volume', 'functional residual capacity', 'inspiratory capacity', 'minute alveolar ventilation'],
+    answer: 'functional residual capacity'
+  },
 
-    {
-      question: "Which of the following is the best way to assess the effectiveness of external chest compressions?",
-      options: [
-        "observe the patient's chest excursion", 
-        "check the patient nail bed color", 
-        "palpate the patient's carotid pulse", 
-        "obtaining an ABG"
-      ],
-      answer: "palpate the patient's carotid pulse"
-    },
+  {
+    question: 'Which of the following is the best way to assess the effectiveness of external chest compressions?',
+    options: [
+      "observe the patient's chest excursion",
+      'check the patient nail bed color',
+      "palpate the patient's carotid pulse",
+      'obtaining an ABG'
+    ],
+    answer: "palpate the patient's carotid pulse"
+  },
 
-    {
-      question: "Following endotracheal intubation of a patient in the ED, what of the following should the RT use to confirm proper tube placement?",
-      options: [
-        "galvanic analyzer", 
-        "mass spectrometer", 
-        "calorimetric capnometer", 
-        "severinghaus electrode"
-      ],
-      answer: "calorimetric capnometer"
-    },
+  {
+    question:
+      'Following endotracheal intubation of a patient in the ED, what of the following should the RT use to confirm proper tube placement?',
+    options: ['galvanic analyzer', 'mass spectrometer', 'calorimetric capnometer', 'severinghaus electrode'],
+    answer: 'calorimetric capnometer'
+  },
 
-    {
-      question: "A spontaneously breathing adult patient is intubated with a 6.0 mm ID endotracheal tube. Which of the following would decrease the patient's work of breathing imposed by the ET tube?",
-      options: [
-        "CPAP", 
-        "pressure support", 
-        "flow-by", 
-        "inspiratory pause"
-      ],
-      answer: "pressure support"
-    },
+  {
+    question:
+      "A spontaneously breathing adult patient is intubated with a 6.0 mm ID endotracheal tube. Which of the following would decrease the patient's work of breathing imposed by the ET tube?",
+    options: ['CPAP', 'pressure support', 'flow-by', 'inspiratory pause'],
+    answer: 'pressure support'
+  },
 
-    {
-      question: "A patient's V/Q scan reveals normal ventilation with absent perfusion in the left lower lobe. These results suggest:",
-      options: [
-        "right to left shunt", 
-        "pulmonary embolism", 
-        "pneumonia", 
-        "hemothorax"
-      ],
-      answer: "pulmonary embolism"
-    },
+  {
+    question:
+      "A patient's V/Q scan reveals normal ventilation with absent perfusion in the left lower lobe. These results suggest:",
+    options: ['right to left shunt', 'pulmonary embolism', 'pneumonia', 'hemothorax'],
+    answer: 'pulmonary embolism'
+  },
 
-    {
-      question: "Which of the following most consistently delivers the highest water vapor content to a patient's airway?",
-      options: [
-        "jet nebulizer", 
-        "bubble humidifier", 
-        "heated wick humidifier", 
-        "hea moisture exchanger"
-      ],
-      answer: "heated wick humidifier"
-    },
+  {
+    question:
+      "Which of the following most consistently delivers the highest water vapor content to a patient's airway?",
+    options: ['jet nebulizer', 'bubble humidifier', 'heated wick humidifier', 'hea moisture exchanger'],
+    answer: 'heated wick humidifier'
+  },
 
-    {
-      question: "This is a deflation of the alveoli of the lung which results in reduced gas exchange (terminology)",
-      options: [
-        "atelectasis", 
-        "pleural effusion", 
-        "asthma", 
-        "respiratory failure"
-      ],
-      answer: "atelectasis"
-    },
+  {
+    question: 'This is a deflation of the alveoli of the lung which results in reduced gas exchange (terminology)',
+    options: ['atelectasis', 'pleural effusion', 'asthma', 'respiratory failure'],
+    answer: 'atelectasis'
+  },
 
-    {
-      question: "An acute respiratory syndrome seen in children and infants characterized by a barking cough (terminology)",
-      options: [
-        "cyanosis", 
-        "croup", 
-        "dyspnea", 
-        "cystic fibrosis"
-      ],
-      answer: "croup"
-    },
+  {
+    question:
+      'An acute respiratory syndrome seen in children and infants characterized by a barking cough (terminology)',
+    options: ['cyanosis', 'croup', 'dyspnea', 'cystic fibrosis'],
+    answer: 'croup'
+  },
 
-    {
-      question: "An abnormal condition in which the skin turns a bluish color caused by decreased oxygen and increased carbon dioxide in the blood (terminology)",
-      options: [
-        "hyperpnea", 
-        "cyanosis", 
-        "dyspnea", 
-        "empyema"
-      ],
-      answer: "cyanosis"
-    },
+  {
+    question:
+      'An abnormal condition in which the skin turns a bluish color caused by decreased oxygen and increased carbon dioxide in the blood (terminology)',
+    options: ['hyperpnea', 'cyanosis', 'dyspnea', 'empyema'],
+    answer: 'cyanosis'
+  },
 
-    {
-      question: "This medical apparatus is inserted through the mouth and trachea to establish an airway: (terminology)",
-      options: [
-        "IPPB", 
-        "eustachian tube", 
-        "Heimlich tube", 
-        "endotracheal tube"
-      ],
-      answer: "endotracheal tube"
-    },
+  {
+    question: 'This medical apparatus is inserted through the mouth and trachea to establish an airway: (terminology)',
+    options: ['IPPB', 'eustachian tube', 'Heimlich tube', 'endotracheal tube'],
+    answer: 'endotracheal tube'
+  },
 
-    {
-      question: 'The prefix in the word, "anoxia", means (terminology)',
-      options: [
-        "oxygen", 
-        "with", 
-        "without", 
-        "air"
-      ],
-      answer: "without"
-    },
+  {
+    question: 'The prefix in the word, "anoxia", means (terminology)',
+    options: ['oxygen', 'with', 'without', 'air'],
+    answer: 'without'
+  },
 
-    {
-      question: "Abnormal respiratory sounds characterized by fine crackles: (terminology)",
-      options: [
-        "dyspnea", 
-        "rhonchi", 
-        "wheezing", 
-        "rales"
-      ],
-      answer: "rales"
-    }
+  {
+    question: 'Abnormal respiratory sounds characterized by fine crackles: (terminology)',
+    options: ['dyspnea', 'rhonchi', 'wheezing', 'rales'],
+    answer: 'rales'
+  }
 ];
